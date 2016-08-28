cd93897e9ea1be0efe109e6060338dbf6cf8fb33
NIghtmechanic <rankatz@gmail.com>
2016-08-29 00:48:29 +0300

    Version 0.1
    Written, compiles (on CCS 6.1), not tested on any HW

---
+++ FW/cricket.h
@@ -0,0 +1,46 @@
+/*
+cricket.h file for the cricket project
+The MIT License (MIT)
+
+Copyright (c) 2016 Ran Katz (Nightmechanic)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+const unsigned int PLAY_CYCLE = 	500; //in 1 MHz cycles
+const unsigned int PLAY_DC = 		250; //in 1MHz cycles
+const unsigned int PLAY_PERIOD = 	10;  //in play cycles
+const unsigned int PAUSE_PERIOD =	10;  //in play cycles
+const unsigned int PLAY_LOOPS =		100;
+const unsigned int IDLE_TIME =		20; //in 50msec units
+const unsigned int OUTER_LOOPS =	2; // number of cricket chirps
+const unsigned int MIN_SLEEP =		20*20; //in 50msec units (20 seconds)
+
+
+typedef enum {
+	INIT,
+	START_PLAYING,
+	PLAYING,
+	PAUSE,
+	IDLE,
+	GOTOSLEEP,
+	EXITSLEEP,
+	LOW_BAT
+} program_state;
+
